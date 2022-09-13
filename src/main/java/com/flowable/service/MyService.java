00ee680297db66bce74516accdbe4823f8e7853f
sovisrushain <sovisrushain@gmail.com>
2022-09-13 11:49:38 +0530

    create rest end-point

--- src/main/java/com/flowable/service/MyService.java
+++ src/main/java/com/flowable/service/MyService.java
@@ -4,10 +4,12 @@ import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class MyService {
 
     @Autowired
