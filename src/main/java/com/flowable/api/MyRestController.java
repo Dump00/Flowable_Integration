fc951c80fc7f97bd89744b8795c6984a4999c8dd
sovisrushain <sovisrushain@gmail.com>
2022-09-13 15:09:51 +0530

    create TaskRepresentation dto

--- src/main/java/com/flowable/api/MyRestController.java
+++ src/main/java/com/flowable/api/MyRestController.java
@@ -1,5 +1,6 @@
 package com.flowable.api;
 
+import com.flowable.dto.TaskRepresentation;
 import com.flowable.service.MyService;
 import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,35 +24,10 @@ public class MyRestController {
     @RequestMapping(value="/tasks", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
     public List<TaskRepresentation> getTasks(@RequestParam String assignee) {
         List<Task> tasks = myService.getTasks(assignee);
-        List<TaskRepresentation> dtos = new ArrayList<TaskRepresentation>();
+        List<TaskRepresentation> dtoList = new ArrayList<TaskRepresentation>();
         for (Task task : tasks) {
-            dtos.add(new TaskRepresentation(task.getId(), task.getName()));
+            dtoList.add(new TaskRepresentation(task.getId(), task.getName()));
         }
-        return dtos;
-    }
-
-    static class TaskRepresentation {
-
-        private String id;
-        private String name;
-
-        public TaskRepresentation(String id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-
-        public String getId() {
-            return id;
-        }
-        public void setId(String id) {
-            this.id = id;
-        }
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-
+        return dtoList;
     }
 }
