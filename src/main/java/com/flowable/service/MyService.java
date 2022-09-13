df6d6f1f998a6a6a1a9deebba416b50a5803b512
sovisrushain <sovisrushain@gmail.com>
2022-09-13 11:42:45 +0530

    create MyService

---
+++ src/main/java/com/flowable/service/MyService.java
@@ -0,0 +1,29 @@
+package com.flowable.service;
+
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.task.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class MyService {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Transactional
+    public void startProcess() {
+        runtimeService.startProcessInstanceByKey("oneTaskProcess");
+    }
+
+    @Transactional
+    public List<Task> getTasks(String assignee) {
+        return taskService.createTaskQuery().taskAssignee(assignee).list();
+    }
+
+}
